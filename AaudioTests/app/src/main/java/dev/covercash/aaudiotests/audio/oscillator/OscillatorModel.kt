82d4435efa826a5df1da4292bc58bee648229c68
covercash2 <covercash2@gmail.com>
2019-10-04 22:05:34 -0700

    change theme

--- AaudioTests/app/src/main/java/dev/covercash/aaudiotests/audio/oscillator/OscillatorModel.kt
+++ AaudioTests/app/src/main/java/dev/covercash/aaudiotests/audio/oscillator/OscillatorModel.kt
@@ -1,14 +1,16 @@
 package dev.covercash.aaudiotests.audio.oscillator
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OscillatorModel : ViewModel() {
+    val playing: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
     val frequency: MutableLiveData<Float> by lazy {
         MutableLiveData<Float>()
     }
-    val playing: MutableLiveData<Boolean> by lazy {
-        MutableLiveData<Boolean>()
+    val level: MutableLiveData<Float> by lazy {
+        MutableLiveData<Float>()
     }
 }
